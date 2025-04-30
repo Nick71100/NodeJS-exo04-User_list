7dac310504d41fcfddf5d53a1a81540e78b2da77
Nick71100 <nick.delattre4@gmail.com>
2025-04-30 17:06:38 +0200

    commit

--- src/router/index.routes.js
+++ src/router/index.routes.js
@@ -3,6 +3,7 @@ import path from "path";
 import jsonfile from "jsonfile";
 
 const router = Router();
+const file = path.join(process.cwd(), "src", "data", "users.json");
 
 router.get("/", (req, res) => {
   const file = path.join(process.cwd(), "./src/data/users.json");
@@ -18,9 +19,18 @@ router.get("/authentication/sign-in", (req, res) => {
   res.render("layout/template", { file: "sign-in", error: null });
 });
 
+router.get("/authentication/sign-out", (req, res) => {
+  req.session.destroy();
+  res.clearCookie("connect.sid");
+  res.redirect("/authentication/sign-in");
+});
+
+router.get("/authentication/sign-up", (req, res) => {
+  res.render("layout/template", { file: "sign-up" });
+});
+
 router.post("/authentication/sign-in", (req, res) => {
   const { name, password } = req.body;
-  const file = path.join(process.cwd(), "src", "data", "users.json");
   const users = jsonfile.readFileSync(file);
   const user = users.find(
     (user) => user.name === name && user.password === password
@@ -39,9 +49,30 @@ router.post("/authentication/sign-in", (req, res) => {
   }
 });
 
-router.get("/logout", (req, res) => {
-  req.session.user = null;
-  res.redirect("/");
+router.post("/authentication/sign-up", (req, res) => {
+  const { name, password, age, poste } = req.body;
+  const users = jsonfile.readFileSync(file);
+
+  const existingUser = users.find((user) => user.name === name);
+
+  if (existingUser) {
+    const params = new URLSearchParams({ error: "Nom déjà utilisé" });
+    res.redirect("/authentication/sign-up?" + params.toString());
+    return;
+  }
+
+  const newUser = {
+    id: users.length + 1,
+    name: name,
+    password,
+    age: age,
+    poste: poste,
+  };
+
+  users.push(newUser);
+  jsonfile.writeFileSync(file, users, { spaces: 4, EOL: "\r\n" });
+
+  res.redirect("/authentication/sign-in");
 });
 
 router.get("/*splat", (req, res) => {
